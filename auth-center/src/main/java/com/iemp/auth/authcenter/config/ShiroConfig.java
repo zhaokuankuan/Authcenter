5d73bc61b48fe7456b68b9a8a3bf1f3a131d8dc8
赵宽宽 <zkkuan@szhq.com>
2018-12-06 16:01:33 +0800

    代码提交

--- auth-center/src/main/java/com/iemp/auth/authcenter/config/ShiroConfig.java
+++ auth-center/src/main/java/com/iemp/auth/authcenter/config/ShiroConfig.java
@@ -1,73 +1,77 @@
 package com.iemp.auth.authcenter.config;
 
+import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
+import org.apache.shiro.mgt.DefaultSubjectDAO;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.apache.shiro.mgt.SecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 /**
  * @author :Mr.kk
- * @date: 2018/11/29 18:30
+ * @date: 2018/12/6 11:16
  */
 @Configuration
 public class ShiroConfig {
-    /**
-     * ShiroFilterFactoryBean 处理拦截资源文件问题。
-     * 注意：单独一个ShiroFilterFactoryBean配置是或报错的，因为在初始化ShiroFilterFactoryBean的时候需要注入：SecurityManager
-     * Filter Chain定义说明
-     * 1、一个URL可以配置多个Filter，使用逗号分隔
-     * 2、当设置多个过滤器时，全部验证通过，才视为通过
-     * 3、部分过滤器可指定参数，如perms，roles
-     */
-    @Bean
-    public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
-        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-        // 必须设置 SecurityManager
-        shiroFilterFactoryBean.setSecurityManager(securityManager);
 
+    @Bean("shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //拦截器
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+        // 配置不会被拦截的链接 顺序判断
+        filterChainDefinitionMap.put("/userlogin/**", "anon");
+        filterChainDefinitionMap.put("/**.js", "anon");
+        filterChainDefinitionMap.put("/druid/**", "anon");
+        filterChainDefinitionMap.put("/swagger**/**", "anon");
+        filterChainDefinitionMap.put("/webjars/**", "anon");
+        filterChainDefinitionMap.put("/v2/**", "anon");
+        // 添加自己的过滤器并且取名为jwt
+        Map<String, Filter> filterMap = new HashMap<String, Filter>(1);
+        filterMap.put("jwt", new JwtFilter());
+        shiroFilterFactoryBean.setFilters(filterMap);
+        //<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边
+        filterChainDefinitionMap.put("/**", "jwt");
         // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
-        //访问的是后端url地址为 /login的接口
         shiroFilterFactoryBean.setLoginUrl("/login");
         // 登录成功后要跳转的链接
         shiroFilterFactoryBean.setSuccessUrl("/index");
-        // 未授权界面;
+        //未授权的链接
         shiroFilterFactoryBean.setUnauthorizedUrl("/403");
-        // 拦截器.
-        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
-        // 配置不会被拦截的链接 顺序判断
-        filterChainDefinitionMap.put("/static/**", "anon");
-        filterChainDefinitionMap.put("/ajaxLogin", "anon");
-        filterChainDefinitionMap.put("/userlogin", "anon");
-        // 配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
-        filterChainDefinitionMap.put("/logout", "logout");
-        //配置某个url需要某个权限码
-        filterChainDefinitionMap.put("/hello", "perms[how_are_you]");
-        // 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边
-        // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
-        filterChainDefinitionMap.put("/", "user");
-        filterChainDefinitionMap.put("/**", "authc");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-        System.out.println("Shiro拦截器工厂类注入成功");
         return shiroFilterFactoryBean;
     }
 
-    @Bean
+    @Bean("securityManager")
     public SecurityManager securityManager() {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        // 设置realm.
-        securityManager.setRealm(myShiroRealm());
+        securityManager.setRealm(myRealm());
+        /*
+         * 关闭shiro自带的session，详情见文档
+         * http://shiro.apache.org/session-management.html#SessionManagement-StatelessApplications%28Sessionless%29
+         */
+        DefaultSubjectDAO subjectDAO = new DefaultSubjectDAO();
+        DefaultSessionStorageEvaluator defaultSessionStorageEvaluator = new DefaultSessionStorageEvaluator();
+        defaultSessionStorageEvaluator.setSessionStorageEnabled(false);
+        subjectDAO.setSessionStorageEvaluator(defaultSessionStorageEvaluator);
+        securityManager.setSubjectDAO(subjectDAO);
         return securityManager;
     }
+
     /**
      * 身份认证realm; (这个需要自己写，账号密码校验；权限等)
      * @return
      */
     @Bean
-    public MyShiroRealm myShiroRealm() {
-        MyShiroRealm myShiroRealm = new MyShiroRealm();
-        return myShiroRealm;
+    public MyRealm myRealm() {
+        MyRealm myRealm = new MyRealm();
+        return myRealm;
     }
+
 }
--- auth-center/src/main/java/com/iemp/auth/authcenter/controller/LoginController.java
+++ auth-center/src/main/java/com/iemp/auth/authcenter/controller/LoginController.java
@@ -1,14 +1,18 @@
 package com.iemp.auth.authcenter.controller;
 
+import com.iemp.auth.authcenter.common.JwtUtil;
+import com.iemp.auth.authcenter.common.ReturnModel;
 import com.iemp.auth.authcenter.domain.User;
+import com.iemp.auth.authcenter.domain.config.JwtToken;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,70 +24,82 @@ import javax.servlet.http.HttpSession;
  * @date: 2018/11/30 13:27
  */
 @RestController
-public class LoginController {
+@Slf4j
+public class LoginController{
 
+
+    /** 登陆
+     * @param user
+     * @return
+     */
     @RequestMapping(value="/userlogin")
-    public String userLogin(Model model, User user, HttpServletResponse response){
-        if(user==null){return "login";}
+    @ResponseBody
+    public ReturnModel userLogin(User user){
+        ReturnModel returnModel = new ReturnModel();
+        if(user==null){
+            returnModel.setMsg("登录失败");
+            return returnModel;
+        }
         String account=user.getLoginName();
         String password=user.getPassword();
-        UsernamePasswordToken token = new UsernamePasswordToken(account,password,false);
+        String token = JwtUtil.sign(account,password);
+        JwtToken jwtToken = new JwtToken(token);
         Subject currentUser = SecurityUtils.getSubject();
         try {
-            currentUser.login(token);
+            currentUser.login(jwtToken);
+            returnModel.addDefaultModel("TOKEN", token);
+            returnModel.setMsg("登录成功！");
+            returnModel.setSuccess(true);
             //此步将 调用realm的认证方法
         } catch(IncorrectCredentialsException e){
             //这最好把 所有的 异常类型都背会
-            model.addAttribute("message", "密码错误");
-            return "login";
+            returnModel.setMsg("账号或者密码错误！");
+            return returnModel;
         } catch (AuthenticationException e) {
-            model.addAttribute("message", "登录失败");
-            return "login";
+            returnModel.setMsg("登录失败！");
+            return returnModel;
         }
-        return "index";
-    }
-
-    //配合shiro配置中的默认访问url
-    @RequestMapping(value="/login")
-    public String getLogin(HttpServletRequest request, Model model, HttpSession session, HttpServletResponse response){
-        return "login";
-    }
-
-    @RequestMapping(value="/hello")
-    public String hello(){
-        return "NewFile";
-    }
-
-    @RequestMapping(value="/")
-    public String index(){
-        System.out.println("访问了后端 /  请求");
-        return "login";
+        return returnModel;
     }
 
     /**
      * 退出
      * @return
      */
-    @RequestMapping(value="logout",method = RequestMethod.GET)
+    @RequestMapping(value="/logout",method =RequestMethod.GET)
     public String logout(HttpServletRequest request){
-
         //subject的实现类DelegatingSubject的logout方法，将本subject对象的session清空了
         //即使session托管给了redis ，redis有很多个浏览器的session
         //只要调用退出方法，此subject的、此浏览器的session就没了
         try {
+            User user = (User) SecurityUtils.getSubject().getPrincipal();
+            String userId=user.getId();
             //退出
             SecurityUtils.getSubject().logout();
 
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            log.error(e.getMessage());
         }
-        return "login";
-
+        return  "redirect:http://127.0.0.1:8080/login";
     }
 
-    @RequestMapping(value="403")
-    public String unAuth(){
+    //配合shiro配置中的默认访问url
+    @RequestMapping(value="/login",method= RequestMethod.GET)
+    public String getLogin(HttpServletRequest request, Model model, HttpSession session, HttpServletResponse response){
+        return "login";
+    }
 
+
+    @RequestMapping(value="/index",method=RequestMethod.GET)
+    public String index(){
+        log.info("访问了后端 /index  请求");
+        return "index";
+    }
+
+    @RequestMapping(value="/error",method=RequestMethod.GET)
+    public String unAuth(){
         return "403";
     }
+
+
 }
